8c93202e0092e377b4f152cffcbe33430f2f0be6
apurvM790 <97803586+apurvM790@users.noreply.github.com>
2025-03-13 11:44:59 +0530

    updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,28 @@
 import { useSelector } from "react-redux";
+import { IoArrowUndoOutline } from "react-icons/io5";
 
 import CartCard from "./CartCard";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Cart = ()=>{
 
     const products = useSelector((store)=>store.cart);
-    if(products.length == 0){
-        return null;
+    const navigate = useNavigate();
+    const handleNavigateClick = ()=>{
+        navigate("/");
     }
+
+    if (products.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center h-52">
+                <h2 className="text-xl font-semibold text-gray-500">Your cart is empty!</h2>
+                <p className="text-gray-400">Add some items to see them here.</p>
+                <button className="rounded-lg border border-green-700 w-28 flex justify-center my-2 text-gray-300 text-xl bg-green-500 px-4 py-2" onClick={handleNavigateClick}><IoArrowUndoOutline /></button>
+            </div>
+        );
+    }
+
     const [cartItems, setCartItems] = useState(products);
     const [totalCartCost, setTotalCartCost] = useState(()=>
         cartItems.reduce((total,item)=>total+parseFloat(item.price),0)
@@ -28,7 +42,7 @@ const Cart = ()=>{
                     <div className="bg-green-600 text-gray-200 font-thin text-xs px-1 py-1 rounded-lg">
                         ${totalCartCost.toFixed(2)}
                     </div>
-            </div>
+            </div>  
         </div>
     )
 }
